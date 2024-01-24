f219938d50c4c3ff77a86b6114471494ee2535d3
Nickson Kiprotich <kiprotichnickson20@gmail.com>
2024-01-24 18:32:04 +0300

    removed blank lines in accordance to pep8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,6 @@ number of subscribers for a given subreddit
 """
 
 from requests import get
-
 def number_of_subscribers(subreddit):
     """
     function that queries the Reddit API and returns the number of subscribers
@@ -13,7 +12,6 @@ def number_of_subscribers(subreddit):
 
     if subreddit is None or not isinstance(subreddit, str):
         return 0
-    
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
